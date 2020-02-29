10b1851a6012125adcdbe147237187e764e8c0a3
Amandabuch <amandalbuch@gmail.com>
2020-02-29 13:29:24 -0800

    created game logic

--- memory-game/js/main.js
+++ memory-game/js/main.js
@@ -2,25 +2,34 @@
 
 const cards = ["queen", "queen", "king", "king"];
 const cardsInPlay = [];
-var cardOne = cards[0];
 
-cardsInPlay.push(cardOne);
-console.log(cardsInPlay[cardsInPlay.length-1]);
-console.log("User flipped queen");
+function checkForMatch(){
+	if (cardsInPlay[0] === cardsInPlay[1]) {
+  	console.log("You found a match!");
+	} else {
+  	console.log("Sorry, try again.");
+	}
+}
+
+function flipCard(cardId){
+	console.log("User flipped " + cards[cardId]);
+	cardsInPlay.push(cards[cardId]);
+	checkForMatch();
+
+}
+
+flipCard(0);
+flipCard(2);
 
 
 
-var cardTwo = cards[2];
-cardsInPlay.push(cardTwo);
 
-console.log(cardsInPlay[cardsInPlay.length-1]);
-console.log("User flipped king");
+	// if (cardsInPlay.length === 2 && cardsInPlay[0] === cardsInPlay[1]) {
+	// alert("You found a match!");
+	// } else {
+	// alert("Sorry, try again.");
+	// };
 
 
-if (cardsInPlay.length === 2 && cardsInPlay[0] === cardsInPlay[1]) {
-alert("You found a match!");
-} else {
-alert("Sorry, try again.");
-};
 
 
